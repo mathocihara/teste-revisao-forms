e87ffdd596c602efab959530ec9b2b7de9376abc
mathocihara <matheusyukio.hocihara@gmail.com>
2022-06-17 21:21:11 -0300

    coletando dados de checkbox

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 var botaoDosDadosFinais = document.getElementById("botaoDeEnvioDeDados");
 var botaoCongelarDados = document.getElementById("congelarDados");
-var form =  new FormData(document.querySelector('form'));// perguntar para a karla sobre formdata
-var valoresDoFormulario = form.get("name");
-
 
 // ----------------------------------------------------------
 
@@ -10,9 +7,33 @@ var valoresDoFormulario = form.get("name");
 
 botaoDosDadosFinais.addEventListener('click', function(event){
 event.preventDefault();
-// console.log(valoresDoFormulario);
-});
+var array = [];});
 
 
 
 
+
+
+//aplicar uma class no htlm somente par input de texto
+//aplicar uma class no html para os radios button e verificar quem esta checado, para isso usa o .checked
+//pegar todo os chechbox e verificar quem esta checado .checked
+//selecionar o select e pegar o valor dele
+// dividirr em tres funcoes
+// estudar setar atributos; onread
+
+
+
+
+// VALOR CHEKADO DO RADIO
+//             <script>
+// function myFunction() {
+//   var checkBox = document.getElementById("myCheck");
+//   var text = document.getElementById("text");
+//   if (checkBox.checked == true){
+//     text.style.display = "block";
+//   } else {
+//      text.style.display = "none";
+//   }
+// }
+// </script>
+        
